ca45b782c62c367445c197e4abc84d6881aca97b
Alexander Mikhailov <45555229+mikhaia@users.noreply.github.com>
2019-08-09 16:34:03 +0700

    Update In search of json.js

--- In search of json.js
+++ In search of json.js
@@ -1,4 +1,4 @@
-var json = '<c:out value="${basicItemContent}" />';
+var json = '<c:out value="${data}" />';
 
 json = json.replace(/({|, )([a-z_]+)=/g, '$1"$2":');
 // json = json.replace(/":([\s\S]+?)}/g, '":"$1"}');
@@ -8,5 +8,14 @@ json = json.replace(/":"{/g, "\":{");
 json = json.replace(/}",/g, "},");
 
 document.write('<code style="background: yellow">');
+document.write(json);
+document.write('</code>');
+
+/* Second version */
+
+var json = '<c:out value="${data}" />';
+json = json.replace(/([^=,{}\[\]]+)/g, "'$1'");
+json = json.replace(/=/g, ' : ');
+document.write('<code>');
 document.write(json);
 document.write('</code>');
